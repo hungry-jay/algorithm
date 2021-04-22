b0925fcd37fb824dbf434aecfbe814fed1d6a8e3
HungryJay <49271821+hungry-jay@users.noreply.github.com>
2021-04-22 13:36:45 +0900

    Create 완주하지 못한 선수.cpp

---
+++ Programmers/level 1/완주하지 못한 선수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    map<string, int> m;
+    for(int i = 0; i < participant.size(); i++) 
+        m[participant[i]]++;
+    for(int i = 0; i < completion.size(); i++)
+        m[completion[i]]--;
+    for(auto iter = m.begin(); iter != m.end(); iter++) 
+        if(iter->second == 1)
+            return iter->first;
+}
